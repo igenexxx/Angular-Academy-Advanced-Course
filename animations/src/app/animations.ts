bd2729052c4ecc825c06bb16cbefb84a24ce6d17
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2020-12-05 21:58:00 +0300

    animations

---
+++ animations/src/app/animations.ts
@@ -0,0 +1,20 @@
+import { animate, animation, style, transition, trigger, useAnimation } from '@angular/animations';
+
+export const fadeIn = animation([
+  style({ opacity: 0 }),
+  animate('{{ delay }}', style({ opacity: 1 }))
+], {
+  params: {
+    delay: '1000ms',
+  }
+});
+
+export const fadeOut = animation(
+  animate('{{ delay }}', style({ opacity: 0 })),
+  { params: { delay: '1000ms' }}
+);
+
+export const fadeInOut = trigger('fadeInOut', [
+  transition('void => *', useAnimation(fadeIn, { params: { delay: '2000ms' }})),
+  transition('* => void', useAnimation(fadeOut, { params: { delay: '2000ms' }})),
+]);
--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
+import { fadeInOut } from './animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    animations: [fadeInOut],
 })
 export class AppComponent {
+  onAnimationStart(): void {
+    console.log('start');
+  }
 
-
-
+  onAnimationEnd(): void {
+    console.log('end');
+  }
 }
